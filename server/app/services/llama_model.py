af9d4b2fd777699048f284af686fd5b8b8b90b8b
wade-wilson-00 <caffeineaddict00dev@gmail.com>
2026-04-13 21:51:31 +0530

    Added txt files for RAG Pipeline

--- server/app/services/llama_model.py
+++ server/app/services/llama_model.py
@@ -4,18 +4,26 @@ from huggingface_hub import InferenceClient
 
 load_dotenv()
 
-client = InferenceClient(
-    api_key=os.getenv("HUGGING_FACE_API_KEY"),
-)
+class MetaModel:
+    def __init__(self):
+        self.client = InferenceClient(
+            api_key=os.getenv("HUGGING_FACE_API_KEY")
+        )
+    
+    def llm_brain(self, role:str, content:str):
+        self.completion =  self.client.chat.completions.create(
+            model= os.getenv("META_LLAMA_MODEL"),
+            messages=[
+                {
+                "role": "user",
+                "content":str(content)
+                }
+            ],
+        )
 
-completion = client.chat.completions.create(
-    model= os.getenv("META_LLAMA_MODEL"),
-    messages=[
-        {
-            "role": "user",
-            "content": "Tell about yourself?"
-        }
-    ],
-)
+        print(self.completion.choices[0].message.content)
 
-print(client.chat_completion([0].message))
+if __name__ == "__main__":
+
+    model = MetaModel()
+    model.llm_brain(role="user", content="Explain me about MRR in SaaS?")
--- server/app/services/rag_service.py
+++ server/app/services/rag_service.py
@@ -6,5 +6,23 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
 
 load_dotenv()
+
+class RAG_Service:
+    def __init__(self):
+        self.embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview")
+
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=100, 
+            chunk_overlap=0
+        )
+
+        self.vectorstore = Chroma(
+            collection_name="example_collection",
+            embedding_function = self.embeddings,
+            persist_directory=".knowledge_base/chroma_langchain_db",
+        )
+    
+    def load_data(self, file_path:str):
